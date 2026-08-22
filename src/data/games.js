68651d898776539358199fbad0d40a0f357b6df0
Quette-art <marquette.hawkins02@icloud.com>
2026-08-22 11:10:47 -0400

    Include football schedule in game data

--- src/data/games.js
+++ src/data/games.js
@@ -1,7 +1,13 @@
 import { soccerGames } from "./soccerGames.js";
 import { flagFootballGames, flagFootballPlayoffGames } from "./flagFootballGames.js";
+import { footballGames } from "./footballGames.js";
 
-export const games = [...soccerGames, ...flagFootballGames, ...flagFootballPlayoffGames];
+export const games = [
+  ...soccerGames,
+  ...flagFootballGames,
+  ...flagFootballPlayoffGames,
+  ...footballGames,
+];
 
 export const upcomingGames = games;
 
