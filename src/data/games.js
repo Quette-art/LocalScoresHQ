7903ddfc278a2bdbf35af24eecaa4d72893d4b10
Quette-art <marquette.hawkins02@icloud.com>
2026-08-28 01:08:25 -0400

    Apply Aug 28 football schedule audit

--- src/data/games.js
+++ src/data/games.js
@@ -1,11 +1,13 @@
 import { footballGames } from "./footballGamesAug22.js";
 import { marylandFootballGames } from "./marylandFootballGames.js";
 import { applyFootballScheduleCorrections } from "./footballScheduleCorrectionsAug28.js";
+import { applyFootballScheduleAudit } from "./footballScheduleAuditAug28.js";
 
 const correctedFootballGames = applyFootballScheduleCorrections(footballGames);
+const auditedFootballGames = applyFootballScheduleAudit(correctedFootballGames);
 
 export const games = [
-  ...correctedFootballGames,
+  ...auditedFootballGames,
   ...marylandFootballGames,
 ];
 
