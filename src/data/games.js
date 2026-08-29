231ecc0c3f5097f7d28042e6d7e978ed7db2188a
Quette-art <marquette.hawkins02@icloud.com>
2026-08-29 11:23:32 -0400

    Publish verified football finals and unconfirmed results

--- src/data/games.js
+++ src/data/games.js
@@ -1,13 +1,15 @@
 import { footballGames } from "./footballGamesAug22.js";
 import { marylandFootballGames } from "./marylandFootballGames.js";
 import { applyFootballScheduleCorrections } from "./footballScheduleCorrectionsAug28.js";
+import { applyFootballResultsAug29 } from "./footballResultsAug29.js";
 
 const correctedFootballGames = applyFootballScheduleCorrections(footballGames);
-
-export const games = [
+const footballGamesWithResults = applyFootballResultsAug29([
   ...correctedFootballGames,
   ...marylandFootballGames,
-];
+]);
+
+export const games = footballGamesWithResults;
 
 export const upcomingGames = games;
 
