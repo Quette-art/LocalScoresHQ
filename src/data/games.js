42208de91d3c65c7dd9e0b798b58c2bc8e0cbfb8
Quette-art <marquette.hawkins02@icloud.com>
2026-08-29 14:45:53 -0400

    Apply corrected Ballou final on main

--- src/data/games.js
+++ src/data/games.js
@@ -2,14 +2,18 @@ import { footballGames } from "./footballGamesAug22.js";
 import { marylandFootballGames } from "./marylandFootballGames.js";
 import { applyFootballScheduleCorrections } from "./footballScheduleCorrectionsAug28.js";
 import { applyFootballResultsAug29 } from "./footballResultsAug29.js";
+import { applyFootballResultCorrectionsAug29 } from "./footballResultCorrectionsAug29.js";
 
 const correctedFootballGames = applyFootballScheduleCorrections(footballGames);
 const footballGamesWithResults = applyFootballResultsAug29([
   ...correctedFootballGames,
   ...marylandFootballGames,
 ]);
+const footballGamesWithCorrectedResults = applyFootballResultCorrectionsAug29(
+  footballGamesWithResults
+);
 
-export const games = footballGamesWithResults;
+export const games = footballGamesWithCorrectedResults;
 
 export const upcomingGames = games;
 
