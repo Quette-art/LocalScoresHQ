5826d72a0c698ac9f8d9b7eda5b9fa9b3ece6cfe
Quette-art <marquette.hawkins02@icloud.com>
2026-08-30 17:28:10 -0400

    Register custom tracked football badges

---
+++ src/data/customTrackedMascots.js
@@ -0,0 +1,19 @@
+import { teamMascots } from "./teamMascots.js";
+
+// Original LocalScoresHQ-created football badges for tracked programs that did
+// not already have a usable site mark. Keeping these aliases in one place lets
+// TeamMascot use the same artwork on team pages, score cards, game details and
+// standings; initials remain the fallback only when no badge exists.
+Object.assign(teamMascots, {
+  "Friendship Collegiate Academy": "/mascots/custom/friendship-collegiate-custom.svg",
+  "Friendship Collegiate": "/mascots/custom/friendship-collegiate-custom.svg",
+  "St. Albans": "/mascots/custom/st-albans-custom.svg",
+  Bullis: "/mascots/custom/bullis-custom.svg",
+  Landon: "/mascots/custom/landon-custom.svg",
+  "National Christian Academy": "/mascots/custom/national-christian-academy-custom.svg",
+  "Rock Creek Christian Academy": "/mascots/custom/rock-creek-christian-academy-custom.svg",
+  "St. Mary's Ryken": "/mascots/custom/st-marys-ryken-custom.svg",
+  "St. Mary’s Ryken": "/mascots/custom/st-marys-ryken-custom.svg",
+  "St. Vincent Pallotti": "/mascots/custom/st-vincent-pallotti-custom.svg",
+  "Our Lady of Good Counsel": "/mascots/standings/good-counsel.png",
+});
