a9f051986838fd13ddae8f1e1ab358ef2dccfece
Quette-art <marquette.hawkins02@icloud.com>
2026-09-05 15:00:30 -0400

    Apply full Bullis 2026 football schedule

--- src/data/games.js
+++ src/data/games.js
@@ -3,6 +3,7 @@ import { marylandFootballGames } from "./marylandFootballGames.js";
 import { applyFootballScheduleCorrections } from "./footballScheduleCorrectionsAug28.js";
 import { applyFootballScheduleAuditAug29 } from "./footballScheduleAuditAug29.js";
 import { applyFootballScheduleCorrectionsAug30 } from "./footballScheduleCorrectionsAug30.js";
+import { applyBullisFootballScheduleSep5 } from "./bullisFootballScheduleSep5.js";
 import { applyFootballResultsAug29 } from "./footballResultsAug29.js";
 import { applyFootballResultCorrectionsAug29 } from "./footballResultCorrectionsAug29.js";
 import { applyFootballResultCorrectionsAug30 } from "./footballResultCorrectionsAug30.js";
@@ -13,10 +14,13 @@ import { applyFootballUpdatesSep5 } from "./footballUpdatesSep5.js";
 const correctedFootballGames = applyFootballScheduleCorrections(footballGames);
 const auditedFootballGames = applyFootballScheduleAuditAug29(correctedFootballGames);
 const cleanedFootballGames = applyFootballScheduleCorrectionsAug30(auditedFootballGames);
-const footballGamesWithResults = applyFootballResultsAug29([
+const footballGamesWithBullisSchedule = applyBullisFootballScheduleSep5([
   ...cleanedFootballGames,
   ...marylandFootballGames,
 ]);
+const footballGamesWithResults = applyFootballResultsAug29(
+  footballGamesWithBullisSchedule
+);
 const footballGamesWithCorrectedResults = applyFootballResultCorrectionsAug29(
   footballGamesWithResults
 );
