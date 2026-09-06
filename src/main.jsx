d6c1b4159a56805660b5d2b4165150fdfb19d95f
Quette-art <marquette.hawkins02@icloud.com>
2026-09-06 16:00:38 -0400

    Load score date auto-centering

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./data/customTrackedMascots.js";
+import "./autoCenterScoreDate.js";
 import "./App.css";
 import "./mobileSearchFix.css";
 import "./scrollPerformance.css";
