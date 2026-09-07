cf2dbca8e43bedcec9ac80c83832292632837f3b
Quette-art <marquette.hawkins02@icloud.com>
2026-09-06 21:22:39 -0400

    Enable Friendship game details logo fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./data/customTrackedMascots.js";
+import "./friendshipGameDetailsFix.js";
 import "./autoCenterScoreDate.js";
 import "./scoreFeatureEnhancements.js";
 import "./App.css";
@@ -10,6 +11,7 @@ import "./mobileSearchFix.css";
 import "./scrollPerformance.css";
 import "./mobileGameDetailsFix.css";
 import "./friendshipCompactFix.css";
+import "./friendshipGameDetailsFix.css";
 
 // vite-plugin-pwa activates new service workers immediately, but an already
 // open iOS tab keeps running the old JavaScript until the document reloads.
