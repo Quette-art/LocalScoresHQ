202fcd5eb2dc18323748d64a9cac36b28a01aebb
Quette-art <marquette.hawkins02@icloud.com>
2026-09-07 21:02:22 -0400

    Use exact Georgetown Prep crest in TeamMascot mapping

---
+++ src/georgetownProfileLogoOverride.js
@@ -0,0 +1,7 @@
+import { teamMascots } from "./data/teamMascots";
+
+const GEORGETOWN_PREP_CREST =
+  "/mascots/generated/georgetown-prep-crest-fixed.webp?v=exact-crest-ios-2";
+
+teamMascots["Georgetown Prep"] = GEORGETOWN_PREP_CREST;
+teamMascots["Georgetown Preparatory School"] = GEORGETOWN_PREP_CREST;
