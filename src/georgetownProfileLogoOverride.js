1b7ac8b1d0311d165434e1e328cd645467c1840f
Quette-art <marquette.hawkins02@icloud.com>
2026-09-07 21:23:17 -0400

    Point Georgetown Prep override to valid WebP

--- src/georgetownProfileLogoOverride.js
+++ src/georgetownProfileLogoOverride.js
@@ -1,7 +1,7 @@
 import { teamMascots } from "./data/teamMascots";
 
 const GEORGETOWN_PREP_CREST =
-  "/mascots/generated/georgetown-prep-crest-fixed.png?v=exact-crest-ios-3";
+  "/mascots/generated/georgetown-prep-crest-fixed.webp?v=exact-crest-ios-4";
 
 teamMascots["Georgetown Prep"] = GEORGETOWN_PREP_CREST;
 teamMascots["Georgetown Preparatory School"] = GEORGETOWN_PREP_CREST;
