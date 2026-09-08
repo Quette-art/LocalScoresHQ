acbd02c463b3fd2a180e45e4a38014adda189d35
Quette-art <marquette.hawkins02@icloud.com>
2026-09-07 21:07:53 -0400

    Keep Georgetown Prep override on real PNG

--- src/georgetownProfileLogoOverride.js
+++ src/georgetownProfileLogoOverride.js
@@ -1,7 +1,7 @@
 import { teamMascots } from "./data/teamMascots";
 
 const GEORGETOWN_PREP_CREST =
-  "/mascots/generated/georgetown-prep-crest-fixed.webp?v=exact-crest-ios-2";
+  "/mascots/generated/georgetown-prep-crest-fixed.png?v=exact-crest-ios-3";
 
 teamMascots["Georgetown Prep"] = GEORGETOWN_PREP_CREST;
 teamMascots["Georgetown Preparatory School"] = GEORGETOWN_PREP_CREST;
